f90c5aaa04db8a31e5c31139cd18b7990189bab1
Daniel Kersten <daniel@dev-kersten.de>
2026-05-18 19:37:26 +0200

    added urls config in core for offers

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.api.urls')),
+    path('api/', include('offers.api.urls')),
 ]
 
 # Serve media files during development
