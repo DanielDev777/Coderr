030b0e441b28def49a26afdb7efc9211e893d44b
Daniel Kersten <daniel@dev-kersten.de>
2026-05-18 18:30:49 +0200

    added offers serializer

---
+++ backend/offers/api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+class OfferDetailListSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    url = serializers.SerializerMethodField()
+    
+    def get_url(self, obj):
+        return f'/api/offerdetails/{obj.id}/'
+    
+class OfferListSerializer(serializers.Serializer):
+    min_price = serializers.SerializerMethodField()
+    min_delivery_time = serializers.SerializerMethodField()
+    user_details = serializers.SerializerMethodField()
+    details = OfferDetailListSerializer(many=True, read_only=True)
+
+    def get_min_price(self, obj):
+        return obj.min_price()
+    
+    def get_min_delivery_time(self, obj):
+        return obj.min_delivery_time()
+    
+    def get_user_details(self, obj):
+        return {
+            'first_name': obj.user.first_name,
+            'last_name': obj.user.last_name,
+            'username': obj.user.username
+        }
