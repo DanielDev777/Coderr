7f4bdd9ad1f6cf795f70cbb35850d1df92f3766f
Daniel Kersten <daniel@dev-kersten.de>
2026-05-21 05:25:44 +0200

    updated OrderListView for post method

--- backend/orders/api/views.py
+++ backend/orders/api/views.py
@@ -1,15 +1,24 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 
 from orders.models import Order
-from orders.api.serializers import OrderSerializer
+from orders.api.serializers import OrderSerializer, OrderCreateSerializer
+from orders.permissions import IsCustomerUser, IsOrderBusinessUser
 
-class OrderListView(ListAPIView):
-    """API endpoint for listing orders."""
-
-    serializer_class = OrderSerializer
+class OrderListView(ListCreateAPIView):
+    """API endpoint for listing and creating orders."""
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return OrderCreateSerializer
+        return OrderSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated(), IsCustomerUser()]
+        return [IsAuthenticated()]
 
     def get_queryset(self):
         user = self.request.user
