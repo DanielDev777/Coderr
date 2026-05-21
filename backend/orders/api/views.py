1bfd61149e8e2ee03e873a59c95b52abaf2098bf
Daniel Kersten <daniel@dev-kersten.de>
2026-05-21 05:52:45 +0200

    added OrderDetailView + url

--- backend/orders/api/views.py
+++ backend/orders/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.db.models import Q
 
 from orders.models import Order
-from orders.api.serializers import OrderSerializer, OrderCreateSerializer
+from orders.api.serializers import OrderSerializer, OrderCreateSerializer, OrderUpdateSerializer
 from orders.permissions import IsCustomerUser, IsOrderBusinessUser
 
 class OrderListView(ListCreateAPIView):
@@ -26,3 +26,30 @@ class OrderListView(ListCreateAPIView):
         return Order.objects.filter(
             Q(customer_user=user) | Q(business_user=user)
         ).select_related('customer_user', 'business_user', 'offer_detail')
+    
+class OrderDetailView(RetrieveUpdateDestroyAPIView):
+    """API endpoint for retrieving, updating, and deleting specific order."""
+
+    queryset = Order.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == 'PATCH':
+            return OrderUpdateSerializer
+        return OrderSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'PATCH':
+            return [IsAuthenticated(), IsOrderBusinessUser()]
+        elif self.request.method == 'DELETE':
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Order.objects.all()
+        
+        return Order.objects.filter(
+            Q(customer_user=user) | Q(business_user=user)
+        )
